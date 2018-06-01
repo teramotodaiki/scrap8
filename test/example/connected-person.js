29691a97ba01377bd15d2d5c2dfa79cc440bd4c1
teramotodaiki <i101323@gmail.com>
2018-06-01 17:30:24 +0900

    feature: add test

---
+++ test/example/connected-person.js
@@ -0,0 +1,47 @@
+export const row =
+  "HackforPlay, Inc.\tDebusho\tCEO\tDaiki Teramoto\tdteramoto@example.com\t000-0000\tKanazawa Ishikawa\t000-0000-0000\t000-0000-0001\t000-0000-0002\t000-0000-0003\t000-0000-0004\thttp://hackforplay.xyz\t2018/06/01\t1\t\t";
+export const person = {
+  会社名: "HackforPlay, Inc.",
+  部署名: "Debusho",
+  役職: "CEO",
+  氏名: "Daiki Teramoto",
+  "e-mail": "dteramoto@example.com",
+  郵便番号: "000-0000",
+  住所: "Kanazawa Ishikawa",
+  TEL会社: "000-0000-0000",
+  TEL部門: "000-0000-0001",
+  TEL直通: "000-0000-0002",
+  FAX: "000-0000-0003",
+  携帯電話: "000-0000-0004",
+  URL: "http://hackforplay.xyz",
+  名刺交換日: "2018/06/01",
+  Eightでつながっている人: "1",
+  再データ化中の名刺: "",
+  "'?'を含んだデータ": ""
+};
+export const page = {
+  title: "Daiki Teramoto",
+  lines: [
+    "Daiki Teramoto",
+    " [2018/06]/01に名刺交換した人",
+    " [HackforPlay, Inc.]Debusho",
+    " http://hackforplay.xyz",
+    " 役職: [CEO]",
+    " 郵便番号: 000-0000",
+    " 住所: Kanazawa Ishikawa",
+    "",
+    "",
+    "[* 連絡先]",
+    " 携帯電話: 000-0000-0004",
+    " e-mail: dteramoto@example.com",
+    " TEL会社: 000-0000-0000",
+    " FAX: 000-0000-0003",
+    " TEL部門: 000-0000-0001",
+    " TEL直通: 000-0000-0002",
+    "",
+    "",
+    "[* タグ]",
+    "#Eightでつながっている人",
+    ""
+  ]
+};
---
+++ test/parse.js
@@ -0,0 +1,45 @@
+import { expect } from "chai";
+import parse, { toJSON } from "../src/parse";
+import fs from "fs";
+import path from "path";
+import fromPairs from "lodash/fromPairs";
+
+describe("parse", function() {
+  describe("#toJSON()", function() {
+    it("should return correct JSON when the connected person row is present", () => {
+      const { row, person } = require("./example/connected-person");
+      const result = toJSON(row);
+      expect(result).to.deep.equal(person);
+    });
+
+    it("should return corrent JSON when the unknown role row is present", () => {
+      const { row, person } = require("./example/unknown-role");
+      const result = toJSON(row);
+      expect(result).to.deep.equal(person);
+    });
+
+    it("should return unknown 0001 page when the unknown person row is present", () => {
+      const { row, person } = require("./example/unknown-person");
+      const result = toJSON(row);
+      expect(result).to.deep.equal(person);
+    });
+
+    it("should return unknown 0002 page when the unknown person row is present again", () => {
+      const { row, person } = require("./example/unknown-person2");
+      const result = toJSON(row);
+      expect(result).to.deep.equal(person);
+    });
+
+    it("should return unknown 0003 page when the person who has no family name is present", () => {
+      const { row, person } = require("./example/unknown-person3");
+      const result = toJSON(row);
+      expect(result).to.deep.equal(person);
+    });
+
+    it("should return correct JSON page when the unknown person row is present", () => {
+      const { row, person } = require("./example/unknown-place");
+      const result = toJSON(row);
+      expect(result).to.deep.equal(person);
+    });
+  });
+});
